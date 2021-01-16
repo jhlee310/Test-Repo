7422e0cd849e0f7d12bfd311f8a5e2654138a00b
Jaehoon Lee <jhoon@Jaehoonui-MacBookPro.local>
2021-01-16 18:42:34 +0900

    Removed Some Code

--- script.py
+++ script.py
@@ -1,23 +1,8 @@
-import math
-import sys
-from os import rename
-
 import requests
 
 name = input("Your Name? ")
 print("Hello,", name)
 
-# # print(sys.version)
-# print(sys.executable)
-
-# input?
-
-# def greet(who_to_greet):
-#     greeting = 'Hello, {}'.format(who_to_greet)
-#     return greeting
-
-
-# # print('a')
 # # print(greet('World'))
 # # print(greet('Corey'))
 # r = requests.get('https://naver.com')
