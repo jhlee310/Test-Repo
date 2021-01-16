eeea9f00a75ae52fa979a36b5a8119fd2da6bff4
Jaehoon Lee <jhoon@Jaehoonui-MacBookPro.local>
2021-01-16 18:40:40 +0900

    Initial Commit

---
+++ script.py
@@ -0,0 +1,24 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+name = input("Your Name? ")
+print("Hello,", name)
+
+# # print(sys.version)
+# print(sys.executable)
+
+# input?
+
+# def greet(who_to_greet):
+#     greeting = 'Hello, {}'.format(who_to_greet)
+#     return greeting
+
+
+# # print('a')
+# # print(greet('World'))
+# # print(greet('Corey'))
+# r = requests.get('https://naver.com')
+# print(r.status_code)
